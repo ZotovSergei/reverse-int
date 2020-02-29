3e380bbecbd0c8b4f29150ed0cdf372ea2c6d98f
Sergei Zotov <zsv.main@gmail.com>
2020-02-29 14:17:11 +0500

    feat: reverse realization function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    return +[...Math.sqrt(n*n).toString()].reverse().join('')
 }
+
+
+
+// function reverse (n) {
+//     return +[...Math.sqrt(n*n).toString()].reverse().join('')
+// }
+
+
+// console.log(reverse(-263))
+
